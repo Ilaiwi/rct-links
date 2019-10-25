fbf357fdec1ed38f994d3e9017704601736a557f
Ahmad Ilaiwi <ahmad.ilaiwi@gmail.com>
2019-10-25 14:58:26 +0300

    remove dnd

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React from "react";
 import { render } from "react-dom";
 import CustomTimeline from "./CustomTimeline";
 import "react-calendar-timeline/lib/Timeline.css";
-import { DndProvider } from "react-dnd";
-import HTML5Backend from "react-dnd-html5-backend";
 
 const App = () => (
   <div>
-    <DndProvider backend={HTML5Backend}>
-      <CustomTimeline />
-    </DndProvider>
+    <CustomTimeline />
   </div>
 );
 
